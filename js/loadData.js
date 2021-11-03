c58977238d6b6546d647623e48093c414958fa7f
Jongwon <jongwon.shin@cern.ch>
2021-11-03 17:43:15 +0900

    commit test

--- js/loadData.js
+++ js/loadData.js
@@ -12,6 +12,7 @@ function eff2color(x, data) {
   return 'white';
 }
 
+
 function loadRunNumber() {
   var runNumber = '0';
   for ( var ss of new URL(document.URL).search.split('&') ) {
@@ -21,7 +22,6 @@ function loadRunNumber() {
     runNumber = ss.substr(4);
   }
 
-  //d3.select('#input_subdir').selectAll('option').remove();
   d3.select('#input_runs').selectAll('option').remove();
   d3.tsv("data/common/runs.tsv")
     .then(function(data) {
@@ -33,6 +33,7 @@ function loadRunNumber() {
         .attr('selected', 'selected');
     });
 }
+
 
 function loadData() {
   var sel = document.getElementById('input_runs');
@@ -48,56 +49,92 @@ function loadData() {
   //  .style('fill', 'white')
   //d3.select('#canvas_endcapMinus').selectAll('polygon')
   //  .style('fill', 'white')
+  d3.select("#canvas_histFCE")
+    .text("Histogram")
+    .append("svg").attr("width", "1200px").attr("height", "30px")
+    .attr("id", "histogram");
 
+  var margin = {top: 10, right: 30, bottom: 30, left: 40},
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+  const svg = d3.select("#canvas_histFCE")
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", `translate(${margin.left},${margin.top})`);
   
-  d3.tsv('data/tsv/'+pfx+'/SummaryAnalyzeEfficiency_'+runNumber+'_Express2021.tsv')
-  .then(function(effData) {
-    d3.select("#canvas_histFCE")
-      .text("Histogram")
-      .append("svg").attr("width", "1200px").attr("height", "30px")
-      .attr("id", "histogram");
-
-    var FCEHistChart = new dc.BarChart("#canvas_histFCE");
-    var effDataCF = crossfilter(effData),
-    FCEDim = effDataCF.dimension(function(d) {return Math.floor(d.Fiducial_Cut_Efficiency/5);}),
-    FCEHist = FCEDim.group().reduceCount();
-    
-    FCEHistChart
-    .dimension(FCEDim)
-    .group(FCEHist)
-    .x(d3.scaleLinear().domain([0,20]))
-    .elasticY(true)
-    .controlsUseVisibility(true);
-    
-    FCEHistChart.xAxis().tickFormat(function(d) {return d*5}); // convert back to base unit
-    FCEHistChart.yAxis().ticks(20);
-    dc.renderAll();
-
-    d3.select('#input_var').attr('value', "Fiducial Cut Efficiency")
-    d3.select('#canvas_barrel').selectAll('polygon')
-    .style('fill', x => eff2color(x, effData));
-    d3.select('#canvas_barrelZPhi').selectAll('polygon')
-    .style('fill', x => eff2color(x, effData));
-    d3.select('#canvas_endcapPlus').selectAll('polygon')
-    .style('fill', x => eff2color(x, effData));
-    d3.select('#canvas_endcapMinus').selectAll('polygon')
-    .style('fill', x => eff2color(x, effData));
-  });
-  
-  d3.select("#canvas_legend")
+  d3.tsv('data/tsv/'+pfx+'/SummaryAnalyzeEfficiency_'+runNumber+'_Express2021.tsv').then(function(effData) {
+    d3.select("#canvas_legend")
     .text("Legend")
     .append("svg").attr("width", "1200px").attr("height", "30px")
     .attr("id", "legend");
-  
-  var colorList = ["#2B0", "#6C0", "#8B0", "#DC0", "#D80", "#800", "white"];
-  var labelList = ["100", "95 ~ 99", "90 ~ 94", "89 ~ 80", "60 ~ 79", "0 ~ 60", "Null"];
-  var svg = d3.select("#legend");
-  for (i=0; i<colorList.length; i++) {
-    svg.append("rect").attr("width", "40px").attr("height", "16px")
-       .attr("x", String(70*i + 360) + "px").attr("y", "10px").style("fill", colorList[i]).style("stroke", "#777");
-    svg.append("text").attr("x", String(70*i + 380)+"px").attr("y", "18px").text(labelList[i])
-       .style("font-size", "10px").attr("alignment-baseline", "middle").attr("text-anchor", "middle");
-  };
-  
+    
+    var colorList = ["#2B0", "#6C0", "#8B0", "#DC0", "#D80", "#800", "white"];
+    var labelList = ["100", "95 ~ 99", "90 ~ 94", "89 ~ 80", "60 ~ 79", "0 ~ 60", "Null"];
+    var LegndSvg = d3.select("#legend");
+    for (i=0; i<colorList.length; i++) {
+      LegndSvg.append("rect").attr("width", "40px").attr("height", "16px")
+         .attr("x", String(70*i + 360) + "px").attr("y", "10px").style("fill", colorList[i]).style("stroke", "#777");
+      LegndSvg.append("text").attr("x", String(70*i + 380)+"px").attr("y", "18px").text(labelList[i])
+         .style("font-size", "10px").attr("alignment-baseline", "middle").attr("text-anchor", "middle");
+    };
+
+
+
+    const x = d3.scaleLinear()
+                .domain([0, d3.max(effData, function(d) { return +d.Fiducial_Cut_Efficiency})])
+                .range([0, width]);
+
+    svg.append("g")
+       .attr("transform", `translate(0, ${height})`)
+       .call(d3.axisBottom(x));
+
+    const histogram = d3.histogram()
+    .value(function(d) { return d.Fiducial_Cut_Efficiency; })   // I need to give the vector of value
+    .domain(x.domain())  // then the domain of the graphic
+    .thresholds(x.ticks(50)); // then the numbers of bins
+ 
+    // And apply this function to data to get the bins
+    const bins = histogram(effData);
+      
+    // Y axis: scale and draw:
+    const y = d3.scaleLinear()
+                .range([height, 0]);
+    //y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+    y.domain([0, 300]);   // d3.hist has to be called before the Y axis obviously
+    svg.append("g")
+       .call(d3.axisLeft(y));
+
+    // append the bar rectangles to the svg element
+    svg.selectAll("rect")
+    .data(bins)
+    .join("rect")
+    .attr("x", 1)
+    .attr("transform", function(d) { return `translate(${x(d.x0)}, ${y(d.length)})`})
+    .attr("width", function(d) { return x(d.x1) - x(d.x0)-1})
+    .attr("height", function(d) { return height - y(d.length); })
+    .style("fill", function(d){ if(d.x0<140){return "orange"} else {return "#69b3a2"}})
+    .style("fill", function(d){
+      if (d.x0 >= 100)   { return colorList[0] } 
+      else if (d.x0 > 95) { return colorList[1] }
+      else if (d.x0 > 90) { return colorList[2] }
+      else if (d.x0 > 80) { return colorList[3] }
+      else if (d.x0 > 60) { return colorList[4] }
+      else if (d.x0 > 0)  { return colorList[5] }
+      else { return "white" }
+    })
+
+
+    d3.select('#input_var').attr('value', "Fiducial Cut Efficiency")
+    d3.select('#canvas_barrel').selectAll('polygon')
+      .style('fill', x => eff2color(x, effData));
+    d3.select('#canvas_barrelZPhi').selectAll('polygon')
+      .style('fill', x => eff2color(x, effData));
+    d3.select('#canvas_endcapPlus').selectAll('polygon')
+      .style('fill', x => eff2color(x, effData));
+    d3.select('#canvas_endcapMinus').selectAll('polygon')
+      .style('fill', x => eff2color(x, effData));
+  });
 }
 
